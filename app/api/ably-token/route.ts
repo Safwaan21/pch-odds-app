38386a33f529cc205f095823318224566f8bf41c
saf <safwaanshaikh21@gmail.com>
2025-03-07 00:05:33 -0500

    Fix build errors and clean up dependencies

--- app/api/ably-token/route.ts
+++ app/api/ably-token/route.ts
@@ -3,16 +3,12 @@ import * as Ably from 'ably';
 
 export async function GET() {
   try {
-    const client = new Ably.Realtime(process.env.ABLY_API_KEY as string);
-    const tokenRequest = await new Promise<any>((resolve, reject) => {
-      client.auth.createTokenRequest({ clientId: 'pch-odds-app-' + Math.random().toString(36).substring(2, 15) }, null, (err, tokenRequest) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(tokenRequest);
-        }
-      });
-    });
+    // Create an Ably REST client
+    const client = new Ably.Rest(process.env.ABLY_API_KEY as string);
+    
+    // Create a token request
+    const tokenParams = { clientId: 'pch-odds-app-' + Math.random().toString(36).substring(2, 15) };
+    const tokenRequest = await client.auth.createTokenRequest(tokenParams);
     
     return NextResponse.json(tokenRequest);
   } catch (error) {
